f0dace9750f8b98c4d95324d47838462039fd744
Jose Renteria <JoseRenteria.sf@outlook.com>
2018-02-17 11:47:56 -0800

    pushing first cmmit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+      <h1>Hello, I am Jose Renteria.<br/>
+      I am a full-stack web developer</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
